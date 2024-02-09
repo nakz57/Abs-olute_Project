c860f852a2bb7123e87955bf122b18a4ef7c3287
AliElamir <ali.elamiir.a@gmail.com>
2024-02-09 22:30:19 +0300

    add opinion routes

--- Absolute/Absolute-Proj/routes/opinions.js
+++ Absolute/Absolute-Proj/routes/opinions.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const opinionsCtrl = require('../controllers/opinions')
 
-// POST /programs/:id/opinions (creating an opinion for a specific program id)
+// POST /programs/:id/opinions (creating an opinion for a specific program id//
 
 router.post('/programs/:id/opinion', opinionsCtrl.create)
 
