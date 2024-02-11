edeb9c5012e5a25ff874690b78c2ae5861244b5a
AbdulAmir1 <abdulamir0@gmail.com>
2024-02-11 11:14:16 +0300

    user/show.ejs created

--- Absolute/Absolute-Proj/routes/users.js
+++ Absolute/Absolute-Proj/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const userCtrl = require('../controllers/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  // res.send('respond with a resource');
+  res.render('user/index')
+})
 
-module.exports = router;
+/* GET user details */
+router.get('/:id', userCtrl.show)
+
+module.exports = router
