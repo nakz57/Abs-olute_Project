58a4a39a79162e0d76e08918cd7b536f19c26ccc
AliElamir <ali.elamiir.a@gmail.com>
2024-02-14 09:10:27 +0300

    updated user routing

--- Absolute/Absolute-Proj/models/user.js
+++ Absolute/Absolute-Proj/models/user.js
@@ -13,7 +13,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   avatar: { type: String, required: true },
-  googleI: {
+  googleId: {
     type: String,
     required: true
   },
